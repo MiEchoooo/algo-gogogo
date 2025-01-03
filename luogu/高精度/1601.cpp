ed42e14e6110318cde3c14e4cd0ff9292fdcd233
不会敲代码的咪🐱 <3525910649@qq.com>
2025-01-03 13:39:37 +0800

    Update 1601.cpp

--- luogu/高精度/1601.cpp
+++ luogu/高精度/1601.cpp
@@ -33,3 +33,54 @@ int main() {
   }
   return 0;
 }
+
+
+/*
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s1, s2;
+  int a1[500] = {0}, a2[500] = {0}, a3[501] = {0};
+  cin >> s1;
+  cin >> s2;
+  //getline(cin, s1);
+  //getline(cin, s2);
+
+  for (int i = s1.size() - 1; i >= 0; i --) {
+    a1[i] = s1[s1.size() - i - 1] - '0';
+  }
+  //for (int i = 0; i < s1.size(); i++) {
+  //  a1[s1.size() - i - 1] = s1[i] - '0';
+  //}
+  for (int i = 0; i <s2.size(); i++) {
+    a2[s2.size() - i - 1] = s2[i] - '0';
+  }
+  int len = s1.size() > s2.size() ? s1.size() : s2.size();
+  for (int i = 0; i < len; i ++) {
+    a3[i] = a1[i] + a2[i];
+  }
+  for (int i = 0; i < len; i ++) {
+    if (a3[i] >= 10) {
+      a3[i + 1] = a3[i + 1] + (a3[i] / 10);
+      a3[i] = a3[i] % 10;
+    }
+  }
+  if (a3[len] != 0) {
+    len ++;
+  }
+ // for (int i = len - 1; i >= 0; i --) {
+ //   if(a3[i] == 0) len--;
+ //   else break;
+ // }
+  if(len == 0) cout << 0; 
+  for(int i = len - 1; i >= 0; i--) {
+    cout << a3[i];
+  }
+  return 0;
+}
+
+*/
